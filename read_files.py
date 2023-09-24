a469a4520eeda46e2ae797374b2727de4be4cd93
David Fernandez Fernandez <david.fernandezf@hotmail.com>
2023-09-25 01:08:04 +0200

    Allow keywords groups

--- read_files.py
+++ read_files.py
@@ -3,21 +3,25 @@ from typing import List
 
 
 def read_user_file(file_name: str) -> List[str]:
-    with open(file_name, 'r') as f:
+    with open(file_name, 'r', encoding='utf-8') as f:
         lines = f.read()
 
     lines = lines.split('\n')
 
     unique_lines = []
     for i in range(len(lines)):
-        line = unidecode(lines[i].lower())  # Normalize unicode characters (e.g. accents)
+        line = unidecode(lines[i].lower()).strip()  # Normalize unicode characters (e.g. accents) and remove spaces
 
-        if len(line) == 0:
-            continue
-        elif line[0] == '#':
-            continue
-        else:
-            if line not in unique_lines:
-                unique_lines.append(line)
+        if line:  # Line is not empty
+            if line[0] == '#':
+                continue
+            else:
+                multiple_keywords = line.split(' + ')
+
+                if len(multiple_keywords) > 1:
+                    unique_lines.append(multiple_keywords)
+                else:
+                    if line not in unique_lines:
+                        unique_lines.append(line)
 
     return unique_lines
---
+++ sort_entries.py
@@ -0,0 +1,153 @@
+from typing import List, Tuple
+from feedparser import FeedParserDict
+from unidecode import unidecode
+
+# Define the strings to be replaced in the markdown file
+TitleEnclosure = ['<span style="color:red"><u>', '</u></span>']
+AuthorEnclosure = ['<span style="color:green"><u>', '</u></span>']
+AbstractEnclosure = ['<span style="color:red"><B>', '</B></span>']
+GroupEnclosure = ['<span style="color:yellow"><B><u>', '</u></B></span>']
+OverlapEnclosure = ['<span style="color:green"><B><u>', '</u></B></span>']
+
+
+def _replace_text(text: str, index_0, index_f, enclosure: List[str]) -> str:
+    text = text[:index_0] + enclosure[0] + text[index_0:index_f] + enclosure[1] + text[index_f:]
+
+    return text
+
+
+def _find_text(text: str, keywords: List[str]) -> Tuple[List[Tuple[int, int, str]], int]:
+    keyword_index = len(keywords) + 1
+    text = text.lower()
+    indices = []
+    for i, keyword in enumerate(keywords):
+        if type(keyword) is str:  # Single keyword
+            index = text.find(keyword)
+            while index != -1:
+                keyword_index = min(keyword_index, i)
+                indices.append((index, index + len(keyword), 'single'))
+                index = text.find(keyword, index + 1)
+        elif type(keyword) is list:  # Group of keywords
+            indices_group = []
+            for keyword_i in keyword:
+                index = text.find(keyword_i)
+                if index == -1:
+                    break
+                indices_temp = []
+                while index != -1:
+                    indices_temp.append((index, index + len(keyword_i), 'group'))
+                    index = text.find(keyword_i, index + 1)
+                indices_group.append(indices_temp)
+            if len(indices_group) == len(keyword):
+                indices += [item for sublist in indices_group for item in sublist]
+
+    indices = sorted(indices, key=lambda x: x[0])
+
+    if keyword_index == len(keywords) + 1:
+        keyword_index = None
+
+    return indices, keyword_index
+
+
+def _find_overlaps(indices):
+    # Check if keyword is inside another keyword
+    indices_to_remove = []
+    for i in range(len(indices)):
+        if i in indices_to_remove:
+            continue
+        index_0_i, index_f_i, kind_i = indices[i]
+        for j in range(i + 1, len(indices)):
+            index_0_j, index_f_j, kind_j = indices[j]
+            index_kept = None
+            if index_0_j > index_f_i:  # Keywords don't overlap
+                break
+            elif index_0_i < index_0_j < index_f_i < index_f_j:
+                # Keywords overlap and i is to the left of j
+                indices_to_remove.append(i)
+                indices_to_remove.append(j)
+                indices.append((index_0_i, index_f_j, 'overlap'))
+                index_kept = len(indices) - 1
+            elif index_0_i <= index_0_j and index_f_i >= index_f_j:  # Keyword j is inside keyword i
+                indices_to_remove.append(j)
+                index_kept = i
+            elif index_0_i >= index_0_j and index_f_i <= index_f_j:  # Keyword i is inside keyword j
+                indices_to_remove.append(i)
+                index_kept = j
+
+            if index_kept is not None:
+                temp = list(indices[index_kept])
+                if kind_i == kind_j:
+                    temp[2] = kind_i
+                else:
+                    temp[2] = 'overlap'
+                indices[index_kept] = tuple(temp)
+
+    indices = [indices[i] for i in range(len(indices)) if i not in indices_to_remove]
+    return indices
+
+
+def _find_title(title: str, keywords: List[str]) -> str:
+    indices, _ = _find_text(title, keywords)
+    indices = _find_overlaps(indices)
+
+    for index_0, index_f, _ in indices[::-1]:
+        title = _replace_text(title, index_0, index_f, TitleEnclosure)
+    return title
+
+
+def _find_keywords(abstract: str, keywords: List[str]) -> Tuple[str, int]:
+    indices, keyword_index = _find_text(abstract, keywords)
+    indices = _find_overlaps(indices)
+
+    for index_0, index_f, kind in indices[::-1]:
+        if kind == 'single':
+            enclosure = AbstractEnclosure
+        elif kind == 'group':
+            enclosure = GroupEnclosure
+        elif kind == 'overlap':
+            enclosure = OverlapEnclosure
+        else:
+            raise ValueError(f'Unknown keyword kind: {kind}')
+
+        abstract = _replace_text(abstract, index_0, index_f, enclosure)
+
+    return abstract, keyword_index
+
+
+def _find_authors(authors_list: str, authors: List[str]) -> Tuple[str, int]:
+    author_index = len(authors) + 1
+    for i, author in enumerate(authors):
+        index_0 = unidecode(authors_list.lower()).find(author)
+
+        if index_0 != -1:
+            index_f = index_0 + len(author)
+            authors_list = _replace_text(authors_list, index_0, index_f, AuthorEnclosure)
+            author_index = min(author_index, i)
+
+    if author_index == len(authors) + 1:
+        author_index = None
+
+    return authors_list, author_index
+
+
+def sort_articles(entries: List[FeedParserDict], keywords: List[str], authors: List[str]) -> List[FeedParserDict]:
+    indices = []
+    max_index = len(keywords) + len(authors) + 1
+    for i in range(len(entries)):
+        title = _find_title(entries[i].title, keywords)
+        abstract, keyword_index = _find_keywords(entries[i].summary, keywords)
+        authors_list, author_index = _find_authors(entries[i].authors, authors)
+
+        if keyword_index is not None:
+            indices.append(keyword_index)
+        elif author_index is not None:
+            indices.append(len(keywords) + author_index)
+        else:
+            indices.append(max_index)
+
+        entries[i].title = title
+        entries[i].summary = abstract
+        entries[i].authors = authors_list
+
+    indices_sorted = [i[0] for i in sorted(enumerate(indices), key=lambda x: x[1])]
+    return [entries[i] for i in indices_sorted]
