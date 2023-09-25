a249ce7b6ec2d23a84e7779ddb8ca4a8803b6c01
David Fernandez Fernandez <david.fernandezf@hotmail.com>
2023-09-25 11:09:11 +0200

    Print the update date

--- format_entries.py
+++ format_entries.py
@@ -1,4 +1,5 @@
 from feedparser import FeedParserDict
+from dates_functions import obtain_data
 
 
 def _fix_title(entry: FeedParserDict):
@@ -54,18 +55,32 @@ def _fix_equations(entry: FeedParserDict):
     entry.summary = abstract
 
 
+def _fix_date(entry: FeedParserDict):
+    date = entry.updated
+
+    ct = obtain_data(date)
+    entry.updated = ct.strftime("%a, %d %b %Y %H:%M:%S GMT")
+
+
 def fix_entry(entry: FeedParserDict):
     _fix_title(entry)
     _fix_abstract(entry)
     _fix_authors(entry)
     _fix_equations(entry)
+    _fix_date(entry)
 
 
 def write_article(entry: FeedParserDict, f, index: int, n_total: int):
     f.write(f'({index + 1} / {n_total})\n\n')
     f.write(f'Title: **{entry.title}**\n\n')
-    f.write(f'Author(s): {entry.authors}\n\n')
+
+    if ',' in entry.authors:
+        f.write(f'Authors: {entry.authors}\n\n')
+    else:
+        f.write(f'Author: {entry.authors}\n\n')
+
     f.write(f'Abstract: {entry.summary}\n\n')
     f.write(f'{entry.id}\n\n')
-    f.write('-' * 50)
+    f.write(f'<span style="font-size:0.9em;">*Updated: {entry.updated}*</span>\n\n')
+    f.write('---')
     f.write('\n\n')
