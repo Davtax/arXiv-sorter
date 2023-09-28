d6a3fdf916037b2be88412a87834df76d614e9d2
David Fernandez Fernandez <david.fernandezf@hotmail.com>
2023-09-28 16:25:15 +0200

    Reformat info messages

--- main.py
+++ main.py
@@ -19,13 +19,14 @@ date_f = datetime.now()
 
 data_found = False
 while not data_found:
-    print('Parsing arXiv API feed between ' + str(date_0.date()) + ' and ' + str(date_f.date()) + '...')
+    print('Parsing arXiv API feed between ' + str(date_0.date()) + ' and ' + str(date_f.date()) + '\n')
     entries_dates, dates = search_entries(categories, date_0, date_f)
     for entries, date in zip(entries_dates, dates):
         if len(entries) == 0:
             continue
 
         data_found = True
+        print('Found ' + str(len(entries)) + ' entries for ' + str(date.date()))
 
         print('Formatting entries...')
         [fix_entry(entry) for entry in entries]
@@ -33,15 +34,13 @@ while not data_found:
         print('Sorting entries...')
         entries = sort_articles(entries, keywords, authors)
 
-        print('Writing entries...')
+        print('Writing entries...\n')
         n_total = len(entries)
         with open(f'abstracts/{date.date()}.md', 'w', encoding='utf-8') as f:
             [write_article(entries[index], f, index, n_total) for index in range(n_total)]
 
             ct = datetime.now()
             f.write(f'\n*This file was created at: {ct.strftime("%d %B %Y %H:%M:%S")}*')
-
-        print('\n')
 
     date_f = date_0
     date_0 -= timedelta(days=1)
