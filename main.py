002ce8b14eebc4a0f0d8a482984217459d174ec3
David Fernandez Fernandez <david.fernandezf@hotmail.com>
2023-10-24 23:26:10 +0200

    Uich, wrong version

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from read_files import read_user_file
 from format_entries import fix_entry, write_article
 from sort_entries import sort_articles
 
+# path = '/Users/david/Library/CloudStorage/OneDrive-UAM/arXiv-sorter/'
+# os.chdir(path)
+
 keywords = read_user_file('keywords.txt')
 authors = read_user_file('authors.txt', sort=True)
 categories = read_user_file('categories.txt', sort=True)
@@ -45,5 +48,5 @@ while not data_found:
     date_f = date_0
     date_0 -= timedelta(days=1)
 
-print('Done with version 0.0.3')
+print('Done with version 0.0.4')
 input('Press ENTER to exit')
