afffb38ce1bfb793f399126e01947bf1997d2f30
David Fernandez Fernandez <david.fernandezf@hotmail.com>
2023-11-03 15:01:34 +0100

    Fail the test

--- tests/same_files.py
+++ tests/same_files.py
@@ -5,6 +5,8 @@ names = sys.argv[1:]
 content = [open(name, 'r').readlines() for name in names]
 content = sorted(content, key=len)
 
+content[0].append('hola que tal')
+
 for i in range(len(content) - 1):
     DF = [x for x in content[i] if x not in content[i + 1]]
     print(DF)
