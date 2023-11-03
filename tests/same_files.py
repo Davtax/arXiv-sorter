76646f6b63ec01f2d92722619a623b739a23f7e0
David Fernandez Fernandez <david.fernandezf@hotmail.com>
2023-11-03 14:50:39 +0100

    Update same_files.py

--- tests/same_files.py
+++ tests/same_files.py
@@ -13,11 +13,20 @@ contentA = open(args.fileA, 'r').readlines()
 contentB = open(args.fileB, 'r').readlines()
 contentC = open(args.fileC, 'r').readlines()
 
-# diff = difflib.ndiff(contentA, contentB)
+diff = difflib.ndiff(contentA, contentB)
 # print('\n'.join(list(diff)))
 
 DF = [x for x in contentA if x not in contentB]
 print(DF)
 
-print(filecmp.cmp(args.fileA, args.fileB))
-print(filecmp.cmp(args.fileB, args.fileC))
+DF = [x for x in contentB if x not in contentA]
+print(DF)
+
+DF = [x for x in contentB if x not in contentC]
+print(DF)
+
+DF = [x for x in contentC if x not in contentB]
+print(DF)
+
+print(filecmp.cmp(args.fileA, args.fileB, shallow=True))
+print(filecmp.cmp(args.fileB, args.fileC, shallow=True))
