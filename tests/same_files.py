6738eb3e1518a06a7ae2f5a6c6d1ddbbcf4ca49d
David Fernandez Fernandez <david.fernandezf@hotmail.com>
2023-11-03 14:31:44 +0100

    Update same_files.py

--- tests/same_files.py
+++ tests/same_files.py
@@ -1,12 +1,19 @@
 import filecmp
 import argparse
+import difflib
 
 parser = argparse.ArgumentParser()
-parser.add_argument('fileA')
+parser.add_argument('files')
 parser.add_argument('fileB')
 parser.add_argument('fileC')
 
 args = parser.parse_args()
+
+contentA = open(args.fileA, 'r').readlines()
+contentB = open(args.fileB, 'r').readlines()
+contentC = open(args.fileC, 'r').readlines()
+
+diff = difflib.ndiff(contentA, contentB)
 
 print(filecmp.cmp(args.fileA, args.fileB))
 print(filecmp.cmp(args.fileB, args.fileC))
