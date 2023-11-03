e7fcc3df2a74823875018dd26da95684add58037
David Fernandez Fernandez <david.fernandezf@hotmail.com>
2023-11-03 15:05:55 +0100

    Remove fail

--- tests/same_files.py
+++ tests/same_files.py
@@ -5,11 +5,9 @@ names = sys.argv[1:]
 content = [open(name, 'r').readlines() for name in names]
 content = sorted(content, key=len)
 
-content[0].append('hola que tal')
-
 for i in range(len(content) - 1):
     DF = [x for x in content[i] if x not in content[i + 1]]
-    print(DF)
 
     if DF:
+        print(f'The following lines are different from machines {DF}')
         sys.exit(1)
